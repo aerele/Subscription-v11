1e56eed462599c321842e3bd58fa0122f7c15dd9
venkat102 <venkatesharunachalam659@gmail.com>
2022-08-09 07:02:00 +0000

    feat: delete the session

--- subscription_v11/__init__.py
+++ subscription_v11/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
-from frappe.sessions import Session
+from frappe.sessions import Session, delete_session
 
 __version__ = '0.0.1'
 
@@ -9,8 +9,7 @@ __version__ = '0.0.1'
 def validate_license():
 	license = frappe.db.get_value("License Subscription Settings", "License Subscription Settings", "license_id")
 	user = frappe.session.user
-	if user != "Administrator":	
-	
+	if user != "Administrator":
 		if not license:
 			frappe.throw("License not found, Contact your Administrator")
 
@@ -28,6 +27,7 @@ def validate_license():
 		if response.status_code == 200:
 			data = response.json()
 			if data['statusCode'] in ['A', 'B', 'C', 'F']:
+				delete_session(frappe.session.sid, frappe.session.user)
 				frappe.throw(data['message'])
 			elif data['statusCode'] == 'D':
 				frappe.msgprint(data['message'])
